165d307697fa6d1c35445de83f9fb1e869f6fb3b
Santiago <santiagofonsecaf311@gmail.com>
2023-03-24 11:11:41 -0500

    main

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import _ from 'lodash';
 
 const iframe = document.querySelector('#vimeo-player');
 
@@ -10,4 +11,9 @@ const onplay = e => {
   localStorage.setItem('videoplayer-current-time', timeUpdate);
 };
 
-player.on('timeupdate', onplay);
+var throttledUpdatePlaybackTime = _.throttle(onplay, 1000);
+let savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime);
+console.log(savedTime);
+
+player.on('timeupdate', throttledUpdatePlaybackTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+const input = document.querySelector('input');
 
+const inputValue = input.addEventListener('input', e => {
+  const inputEmail = input.value;
+  localStorage.setItem('feedback-form-state', inputEmail);
+});
+
+if (localStorage.key !== '') {
+  const loadValue = () => {
+    const saved = localStorage.getItem('feedback-form-state');
+    input.value = saved;
+  };
+  window.onload = loadValue;
+}
+
+input.addEventListener('submit', () => {
+  localStorage.clear;
+});
