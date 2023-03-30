d0a084204e8f841a9d32219365fbb4756e5ec3f6
Santiago <santiagofonsecaf311@gmail.com>
2023-03-30 13:17:04 -0500

    main

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,19 @@
 import { throttle } from 'lodash';
 
-const input = document.querySelector('input');
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('#message');
 const form = document.querySelector('form');
+let inputSaved = {};
 
 const saveFormState = () => {
-  const saved = input.value;
-  localStorage.setItem('feedback-form-state', saved);
+  const saved = {
+    email: emailInput.value,
+    text: messageInput.value,
+  };
+
+  const inputSaved = JSON.stringify(saved);
+  localStorage.setItem('feedback-form-state', inputSaved);
+  console.log(inputSaved);
 };
 
 const throttledSaveFormState = throttle(saveFormState, 500);
@@ -13,7 +21,9 @@ const throttledSaveFormState = throttle(saveFormState, 500);
 if (localStorage.getItem('feedback-form-state') !== null) {
   const loadValue = () => {
     const saved = localStorage.getItem('feedback-form-state');
-    input.value = saved;
+    inputSaved = JSON.parse(saved);
+    emailInput.value = inputSaved.email;
+    messageInput.value = inputSaved.text;
   };
   window.onload = loadValue;
 }
@@ -23,4 +33,5 @@ form.addEventListener('submit', event => {
   localStorage.removeItem('feedback-form-state');
 });
 
-input.addEventListener('input', throttledSaveFormState);
+emailInput.addEventListener('input', throttledSaveFormState);
+messageInput.addEventListener('input', throttledSaveFormState);
